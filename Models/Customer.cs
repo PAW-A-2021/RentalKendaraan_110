0dbd63c00be45ec9061a8b49682f7678740f6a41
wachidmuslih <wachidmslh@gmail.com>
2021-11-09 23:38:44 +0700

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RentalKendaraan_110.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Peminjamen = new HashSet<Peminjaman>();
+        }
+
+        public int IdCostumer { get; set; }
+        public string NamaCostumer { get; set; }
+        public string Nik { get; set; }
+        public string Alamat { get; set; }
+        public string NoHp { get; set; }
+        public int? IdGender { get; set; }
+
+        public virtual Gender IdGenderNavigation { get; set; }
+        public virtual ICollection<Peminjaman> Peminjamen { get; set; }
+    }
+}
